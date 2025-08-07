f0e5c96477b117b3f95c9e36ab5dffdecd5f9c15
immortal521 <immort521@gmail.com>
2025-08-07 17:12:22 +0800

    modified:   install.ps1 	modified:   modules/main.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,10 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
 $zipUrl = "https://github.com/immortal521/init-windows/archive/refs/heads/master.zip"
 $tempDir = "$env:TEMP\gh-setup"
 $zipPath = "$tempDir\scripts.zip"
+$unzipPath = "$tempDir\unzipped"
 
 # 确保临时目录存在
 if (!(Test-Path $tempDir)) {
@@ -11,10 +15,11 @@ if (!(Test-Path $tempDir)) {
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
 
 # 解压
-Expand-Archive -LiteralPath $zipPath -DestinationPath $tempDir -Force
+Expand-Archive -LiteralPath $zipPath -DestinationPath $unzipPath -Force
 
-# 执行主逻辑（普通权限）
-& "$tempDir\modules\main.ps1"
+# 执行主逻辑
+$mainScript = Join-Path $unzipPath "init-windows-master\modules\main.ps1"
+& $mainScript
 
 # 清理
 Remove-Item $tempDir -Recurse -Force
